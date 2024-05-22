ba017f23447d12cec04541a2ca424d2e0f769a0e
youssefrmili <73133432+youssefrmili@users.noreply.github.com>
2024-05-22 09:01:36 +0100

    Create owasp-dependency-check-front.sh

---
+++ owasp-dependency-check-front.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+OWASPDC_DIRECTORY=$HOME/OWASP-Dependency-Check
+DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
+REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
+
+if [ ! -d "$DATA_DIRECTORY" ]; then
+    echo "Initially creating persistent directories"
+    mkdir -p "$DATA_DIRECTORY"
+    chmod -R 777 "$DATA_DIRECTORY"
+
+    mkdir -p "$REPORT_DIRECTORY"
+    chmod -R 777 "$REPORT_DIRECTORY"
+fi
+
+# Make sure we are using the latest version
+
+docker run --rm \
+    --volume $(pwd)/ecomm-ui:/src \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
+    --volume "$REPORT_DIRECTORY":/report \
+    owasp/dependency-check \
+    --scan /src/package-lock.json \
+    --exclude "dependency-check-bin" \
+    --format HTML \
+    --project "Frontend OWASP Dependency Check Project" \
+    --out /report
