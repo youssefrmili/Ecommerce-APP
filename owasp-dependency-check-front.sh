ca2028127fec6ca23baf6a4b3de0fc69d71caa5a
youssefrmili <73133432+youssefrmili@users.noreply.github.com>
2024-06-05 11:29:53 +0100

    Update owasp-dependency-check-front.sh

--- owasp-dependency-check-front.sh
+++ owasp-dependency-check-front.sh
@@ -16,6 +16,7 @@ fi
 # Make sure we are using the latest version
 
 docker run --rm \
+    --user 129:137 \
     --volume $(pwd):/ecomm-front \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
     --volume "$REPORT_DIRECTORY":/report \
