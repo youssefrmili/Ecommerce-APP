133e15be9cad92e73fbe8c1502febbd16a8da471
youssefrmili <73133432+youssefrmili@users.noreply.github.com>
2024-06-10 17:07:45 +0100

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -1,34 +1,25 @@
 #!/bin/sh
 
-DC_VERSION="latest"
-DC_DIRECTORY=$HOME/workspace/**
-DC_PROJECT="dependency-check scan: $(pwd)"
-DATA_DIRECTORY="$DC_DIRECTORY/data"
-CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
-
+OWASPDC_DIRECTORY=$HOME/workspace/**
+DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
+REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
 if [ ! -d "$DATA_DIRECTORY" ]; then
-    echo "Initially creating persistent directory: $DATA_DIRECTORY"
+    echo "Initially creating persistent directories"
     mkdir -p "$DATA_DIRECTORY"
     chmod -R 777 "$DATA_DIRECTORY"
-fi
-if [ ! -d "$CACHE_DIRECTORY" ]; then
-    echo "Initially creating persistent directory: $CACHE_DIRECTORY"
-    mkdir -p "$CACHE_DIRECTORY"
-    chmod -R 777 "$CACHE_DIRECTORY"
+
+    mkdir -p "$REPORT_DIRECTORY"
+    chmod -R 777 "$REPORT_DIRECTORY"
 fi
 
 # Make sure we are using the latest version
-
 docker run --rm \
-    -e user=$USER \
-    -u $(id -u ${USER}):$(id -g ${USER}) \
-    --volume $(pwd):/src:z \
-    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
-    --volume $(pwd)/odc-reports:/report:z \
-    owasp/dependency-check:$DC_VERSION \
+    --volume "`pwd`":/src \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
+    --volume "$REPORT_DIRECTORY":/report \
+    owasp/dependency-check \
     --scan /src \
-    --format "ALL" \
-    --project "$DC_PROJECT" \
+    --format "HTML" \
+    --project "My OWASP Dependency Check Project" \
     --out /report
-    # Use suppression like this: (where /src == $pwd)
-    # --suppression "/src/security/dependency-check-suppression.xml"
+
